c08645552fc4a7a47d24c6bf8a760da32a68d682
Swan <a.dogan@interface3.be>
2025-09-15 12:08:27 +0200

    EquipeFixtures

--- src/DataFixtures/EquipeFixtures.php
+++ src/DataFixtures/EquipeFixtures.php
@@ -10,39 +10,13 @@ class EquipeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $equipes = [
-            [
-                'nom' => 'Paris Saint-Germain',
-                'logo' => 'psg-logo.png',
-                'description' => 'Paris Saint-Germain Football Club, commonly referred to as Paris Saint-Germain, Paris SG or PSG, is a French professional football club based in Paris.'
-            ],
-            [
-                'nom' => 'Olympique de Marseille',
-                'logo' => 'om-logo.png',
-                'description' => 'Olympique de Marseille is a French professional football club in Marseille. Founded in 1899, the club plays in Ligue 1 and have spent most of their history in the top tier of French football.'
-            ],
-            [
-                'nom' => 'Olympique Lyonnais',
-                'logo' => 'ol-logo.png',
-                'description' => 'Olympique Lyonnais is a French professional football club based in Lyon. The club plays in Ligue 1 and has won seven consecutive Ligue 1 titles from 2002 to 2008.'
-            ],
-            [
-                'nom' => 'AS Monaco',
-                'logo' => 'monaco-logo.png',
-                'description' => 'Association Sportive de Monaco Football Club, commonly referred to as AS Monaco or simply Monaco, is a Monégasque professional football club based in Fontvieille.'
-            ],
-            [
-                'nom' => 'Lille OSC',
-                'logo' => 'lille-logo.png',
-                'description' => 'Lille Olympique Sporting Club, commonly referred to as LOSC, LOSC Lille or simply Lille, is a French professional football club based in Lille.'
-            ]
-        ];
+        $equipeData = ['nom' => 'Gentlemates', 'description' => 'Equipe forte', 'logo' => 'logo.png'];
 
-        foreach ($equipes as $equipeData) {
+        foreach ($equipeData as $equipe) {
             $equipe = new Equipe();
-            $equipe->setNom($equipeData['nom']);
-            $equipe->setLogo($equipeData['logo']);
-            $equipe->setDescription($equipeData['description']);
+            $equipe->setNom($equipe['nom']);
+            $equipe->setLogo($equipe['logo']);
+            $equipe->setDescription($equipe['description']);
             
             $manager->persist($equipe);
         }
